dc0e594c33b47dd6509930928c2649f669f956fd
hamzahayatunstuck <hamza.hayat@unstucklabs.com>
2026-03-16 22:16:05 +0500

    feat: fix all build errors and run npm install fix any d...

---
+++ jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+};
+
+export default config;
